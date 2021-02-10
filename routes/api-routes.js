6ba9305ccf37fec37869b9b28828de6b20f8280e
Lucah Endicott <lucahdavid88@gmail.com>
2021-02-09 17:37:06 -0800

    add api routes, get and create

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,3 +1,44 @@
 const db = require("../models");
 
-module.exports = (app) => {};
+module.exports = (app) => {
+  app.get("/api/workouts", (req, res) => {
+    db.Workout.find({})
+
+      .then((dbWorkout) => {
+        res.json(dbWorkout);
+      })
+      .catch((err) => {
+        res.json(err);
+      });
+  });
+
+  //post to create a workout
+  app.post("/api/workouts", (req, res) => {
+    db.Workout.create({})
+      .then((dbWorkout) => {
+        res.json(dbWorkout);
+      })
+      .catch((err) => {
+        res.json(err);
+      });
+  });
+  //get request for workouts range (find)
+
+  app.put("/api/workouts/:id", (req, res) => {
+    db.Workout.findByIdAndUpdate(
+      req.params.id,
+      {
+        $push: { exercise: req.body },
+      },
+      {
+        new: true,
+      }
+    )
+      .then((dbWorkout) => {
+        res.json(dbWorkout);
+      })
+      .catch((err) => {
+        res.json(err);
+      });
+  });
+};
--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3000;
 
-const Workout = require("./models/Workout");
 const app = express();
 
 app.use(logger("dev"));
