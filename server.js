da6be0cc00539fac4964f2bcef3c927589e57800
Lucah Endicott <lucahdavid88@gmail.com>
2021-02-11 15:58:27 -0800

    deploying again

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
-  // useFindAndModify: false
+  useFindAndModify: false
 });
 
 require("./routes/html-routes")(app);
